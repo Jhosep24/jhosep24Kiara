c630b26c5ed6b7e155c48968dff68777484c2e33
Jhosep24 <jhosepcolchado@gmail.com>
2024-12-23 14:02:03 -0500

    Delete js directory

--- js/arbol2.js
+++
@@ -1,6 +0,0 @@
- // Esperar a que se cargue el audio y luego recortarlo para que empiece desde el segundo 8
- window.onload = function() {
-    var musica = document.getElementById('musica');
-    musica.currentTime = 8; // Configurar el tiempo de inicio a 8 segundos
-    musica.play(); // Comenzar la reproducción después de recortar los primeros 8 segundos
-  };
--- js/poema.js
+++
@@ -1,7 +0,0 @@
-// script.js - Agrega funcionalidad adicional si es necesario
-
-// Ejemplo de función que podrías usar para añadir más efectos
-document.addEventListener("DOMContentLoaded", function() {
-    console.log("La página ha cargado correctamente.");
-  });
-  
